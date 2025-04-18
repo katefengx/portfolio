86eea3de0273387ed0816409eab685c82215a3d3
Kate Feng <164668941+katefengx@users.noreply.github.com>
2025-04-18 15:22:21 -0700

    fixing github display on js

--- global.js
+++ global.js
@@ -20,7 +20,7 @@ for (let p of pages) {
     
     const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/"                  // Local server
-    : "/website/";         // GitHub Pages repo name
+    : "/portfolio/";         // GitHub Pages repo name
 
     url = !url.startsWith('http') ? BASE_PATH + url : url;
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
